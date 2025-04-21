abcbe8f5f83d3e3fece739a13cd600033c5c061a
islam_debbi <islamdebbi6@gmail.com>
2025-04-21 21:44:15 +0200

    - update users

--- models/user.js
+++ models/user.js
@@ -8,9 +8,9 @@ class User {
     }
 }
 
-const user1 = new User(1, 'admin', 'adminp', 'true');
-const user2 = new User(2, 'islam', 'islamp', 'false');
-const user3 = new User(3, 'user3', 'user3p', 'false');
+const user1 = new User(1, 'admin', 'pass', 'true');
+const user2 = new User(2, 'islam', 'pass', 'false');
+const user3 = new User(3, 'user3', 'pass', 'false');
 const users = [user1, user2, user3];
 
 module.exports = { users, User };
