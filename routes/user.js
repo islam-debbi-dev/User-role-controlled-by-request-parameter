93a6f0cb0e6b9d5f9cb5ad244fa01526de6cb5f5
islam_debbi <islamdebbi6@gmail.com>
2025-04-21 19:25:22 +0200

    - creaet user router

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,27 @@
 const express = require('express');
-const User = require('../models/user');
+const {users} = require('../models/user');
 const router = express.Router();
+
+
+// Handle delete user
+router.delete('/users/:id', (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(id);
+        const userIndex = users.findIndex(user => user.id === parseInt(id));
+        console.log(userIndex);
+        if (userIndex === -1) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        users.splice(userIndex, 1); // Remove the user from the array
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+
+
+module.exports = router; 
